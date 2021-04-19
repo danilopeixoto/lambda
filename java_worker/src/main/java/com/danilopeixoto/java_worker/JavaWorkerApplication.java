fc18bdfe130096480634c377f3a6f054d7a488ca
Danilo Peixoto Ferreira <danilo.ferreira@inatel.br>
2021-04-19 17:36:04 -0300

    Setup projects

---
+++ java_worker/src/main/java/com/danilopeixoto/java_worker/JavaWorkerApplication.java
@@ -0,0 +1,11 @@
+package com.danilopeixoto.java_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWorkerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWorkerApplication.class, args);
+	}
+}
---
+++ java_worker/src/test/java/com/danilopeixoto/java_worker/JavaWorkerApplicationTests.java
@@ -0,0 +1,10 @@
+package com.danilopeixoto.java_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWorkerApplicationTests {
+	@Test
+	void contextLoads() {}
+}
